14d2381a88c50afb0badb72942374028790c01c4
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-07-12 14:21:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,61 +1,34 @@
 import os
 from pyrogram import Client, filters
-import subprocess
+import shutil
+from os import system as cmd
 bot = Client(
     "myfirs",
     api_id=17983098,
     api_hash="ee28199396e0925f1f44d945ac174f64",
-    bot_token="6280972722:AAG3GrropPJhZvfjljtgppKeeXpfpBVZG4Y"
+    bot_token="5545470781:AAGjzXBuTOeJ1JulON1Cw0I2ZP8kebs2Alk"
 )
 @bot.on_message(filters.command('start') & filters.private)
 def command1(bot,message):
-    bot.send_message(message.chat.id, " السلام عليكم أنا فصل الموسيقا , فقط أرسل الصوتية هنا\n\n  لبقية البوتات هنا \n\n https://t.me/ibnAlQyyim/1120 ",disable_web_page_preview=True)
+    bot.send_message(message.chat.id, " السلام عليكم أنا بوت ضغط الفيديوهات , فقط أرسل الفيديو  هنا\n\n  لبقية البوتات هنا \n\n https://t.me/ibnAlQyyim/1120 ",disable_web_page_preview=True)
     
-@bot.on_message(filters.private & filters.incoming & filters.audio )
+@bot.on_message(filters.private & filters.incoming & filters.document | filters.video )
 def _telegram_file(client, message):
-  try: 
-    with open("./output", 'r') as fh:
-      
-            sent_message = message.reply_text('هناك عملية يتم الآن . أرسل الصوتية بعد مدة من فضلك', quote=True)
-            return
-  except FileNotFoundError: 
-    pass  
+
   user_id = message.from_user.id 
-  sent_message = message.reply_text('جار الفصل \n\n قال رسول الله ﷺ  لَيَكونَنَّ مِن أُمَّتي أقْوامٌ يَسْتَحِلُّونَ الحِرَ والحَرِيرَ، والخَمْرَ والمَعازِفَ، ولَيَنْزِلَنَّ أقْوامٌ إلى جَنْبِ عَلَمٍ، يَرُوحُ عليهم بسارِحَةٍ لهمْ، يَأْتِيهِمْ -يَعْنِي الفقِيرَ- لِحاجَةٍ، فيَقولونَ: ارْجِعْ إلَيْنا غَدًا، فيُبَيِّتُهُمُ اللَّهُ، ويَضَعُ العَلَمَ، ويَمْسَخُ آخَرِينَ قِرَدَةً وخَنازِيرَ إلى يَومِ القِيامَةِ. ( صحيح البخاري)', quote=True)
-  file = message.audio
-  file_path = message.download(file_name="entry")
-
-    # Execute speech.py script with entry file
-  subprocess.call(['spleeter', 'separate', '-p', 'spleeter:2stems', '-o', 'output' , "./downloads/entry" ])
-  subprocess.call(['mv',"./output/entry/vocals.wav" , "./output/entry/vocals.mp3" ])
-    # Upload transcription file to user
-  with open("./output/entry/vocals.mp3", 'rb') as f:
-        bot.send_audio(message.chat.id, f)
-  subprocess.call(['sudo','rm','-r',"output"]) 
- 
- 
-@bot.on_message(filters.private & filters.incoming & filters.voice )
-
-def _telegram_file(client, message):
-  try: 
-    with open("./output", 'r') as fh:
-        
-            sent_message = message.reply_text('هناك عملية  تتم الآن . أرسل الصوتية بعد مدة من فضلك', quote=True)
-            return
-  except FileNotFoundError: 
-    pass  
-  user_id = message.from_user.id
-  sent_message = message.reply_text('جار الفصل \n\n قال رسول الله ﷺ  لَيَكونَنَّ مِن أُمَّتي أقْوامٌ يَسْتَحِلُّونَ الحِرَ والحَرِيرَ، والخَمْرَ والمَعازِفَ، ولَيَنْزِلَنَّ أقْوامٌ إلى جَنْبِ عَلَمٍ، يَرُوحُ عليهم بسارِحَةٍ لهمْ، يَأْتِيهِمْ -يَعْنِي الفقِيرَ- لِحاجَةٍ، فيَقولونَ: ارْجِعْ إلَيْنا غَدًا، فيُبَيِّتُهُمُ اللَّهُ، ويَضَعُ العَلَمَ، ويَمْسَخُ آخَرِينَ قِرَدَةً وخَنازِيرَ إلى يَومِ القِيامَةِ. ( صحيح البخاري)', quote=True)
+  sent_message = message.reply_text('جار الضغط ', quote=True)
   file = message.voice
-  file_path = message.download(file_name="entry")
+  file_path = message.download(file_name="./downloads/")
+  filename = os.path.basename(file_path)
+  cmd(f'mkdir workdir')
+  res = "./workdir/"+filename
+  cmd(f'ffmpeg -i {file_path} -vcodec libx265 -crf 28 {res}')
+  with open(res, 'rb') as f:
+        bot.send_video(message.chat.id, f)
+  shutil.rmtree('./workdir/')
+  shutil.rmtree('./downloads/')
 
-    # Execute speech.py script with entry file
-  subprocess.call(['spleeter', 'separate', '-p', 'spleeter:2stems', '-o', 'output' , "./downloads/entry" ])
-  subprocess.call(['mv',"./output/entry/vocals.wav" , "./output/entry/vocals.mp3" ])
-    # Upload transcription file to user
-  with open('./output/entry/vocals.mp3', 'rb') as f:
-        bot.send_audio(message.chat.id, f)
-  subprocess.call(['sudo','rm','-r',"output"])  
-   
+ 
+ 
 
 bot.run()
